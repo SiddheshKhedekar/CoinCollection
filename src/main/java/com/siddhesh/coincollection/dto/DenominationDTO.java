b266e864e4af9bf430535d0f70732cc84d921866
Siddhesh Khedekar <siddheshkhedekar.dev@gmail.com>
2025-05-14 16:51:48 +0200

    remove unused classes

--- src/main/java/com/siddhesh/coincollection/dto/DenominationDTO.java
+++
@@ -1,9 +0,0 @@
-package com.siddhesh.coincollection.dto;
-
-public record DenominationDTO(
-        Long id,
-        String series,
-        Double value,
-        String imageUrl,
-        Long currencyId
-) {}
